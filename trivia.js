46e8a50bb9bcacc9c7e6a068bee54b3323b043b3
Kali Pulkkinen <kapulkkinen@gmail.com>
2019-11-13 18:25:08 -0500

    additional js file updates

--- trivia.js
+++ trivia.js
@@ -3,148 +3,143 @@ let correct = 0;
 let incorrect = 0;
 let missed = 0;
 
+//Timer
+//let count = 30;
+//let counter = setInterval(timer, 1000);
+
+//Notifications
+let notifications = ["Correct", "Incorrect", "Out of Time", "Game Over"];
+
 let questions = [
-    {
-        q1: "the fingerprints of which animal most resemble humans?",
-        choices: ["cat", "koala", "gorilla", "parrot"],
-        answer: [1]
+    "the fingerprints of which animal most resemble humans?",
+    "what is a group of giraffes known as?",
+    "name the largest mammal of the world",
+    "name the slowest animal on earth",
+    "which animal's eye is bigger than its brain?",
+    "which animal keeps growing until death?",
+    "what do we call a female donkey?",
+    "how many hearts does an octopus have?",
+    "which food is the same size as a newborn kangaroo?",
+    "which animal has the longest lifespan?",
+    "how many times can a hummingbird flat its wings per second?",
+];
 
-    },
-    {
-        q2: "what is a group of giraffes known as?",
-        choices: ["tower", "pride", "herd", "flock"],
-        answer: [0]
+let q1Choice = ["cat", "koala", "gorilla", "parrot"];
+let q2Choice = ["tower", "pride", "herd", "flock"];
+let q3Choice = ["elephant", "great dane", "blue whale", "rhinoceros"];
+let q4Choice = ["snail", "cheetah", "gopher", "sloth"];
+let q5Choice = ["squirrel", "salmon", "ostrich", "raccoon"];
+let q6Choice = ["squid", "lobster", "frog", "shark"];
+let q7Choice = ["jenny", "maggie", "kelly", "susan"];
+let q8Choice = ["3", "7", "1", "4"];
+let q9Choice = ["brussel sprout", "banana", "lima bean", "pineapple"];
+let q10Choice = ["elephant", "blue whale", "gianto tortoise", "locust"];
+let q11Choice = ["20", "40", "80", "160"];
 
-    },
-    {
-        q3: "name the largest mammal of the world",
-        choices: ["elephant", "great dane", "blue whale", "rhinoceros"],
-        answer: [2]
+let answer = ["koala", "tower", "blue whale", "sloth", "ostrich", "lobster", "jenny", "3", "lima bean", "gianto tortoise", "80" ];
 
-    },
-    {
-        q4: "name the slowest animal on earth",
-        choices: ["snail", "cheetah", "gopher", "sloth"],
-        answer: [3]
-
-    },
-    {
-        q5: "which animal's eye is bigger than its brain?",
-        choices: ["squirrel", "salmon", "ostrich", "raccoon"],
-        answer: [3]
-
-    },
-    {
-        q6: "which animal keeps growing until death?",
-        choices: ["squid", "lobster", "frog", "shark"],
-        answer: [1]
-
-    },
-    {
-        q7: "what do we call a female donkey?",
-        choices: ["jenny", "maggie", "kelly", "susan"],
-        answer: [0]
-
-    },
-    {
-        q8: "how many hearts does an octopus have?",
-        choices: ["3", "7", "1", "4"],
-        answer: [0]
-
-    },
-    {
-        q9: "which food is the same size as a newborn kangaroo?",
-        choices: ["brussel sprout", "banana", "lima bean", "pineapple"],
-        answer: [2]
-
-    },
-    {
-        q10: "which animal has the longest lifespan?",
-        choices: ["elephant", "blue whale", "gianto tortoise", "locust"],
-        answer: [2]
-
-    },
-    {
-        q11: "how many times can a hummingbird flat its wings per second?",
-        choices: ["20", "40", "80", "160"],
-        answer: [2]
-
-    },
-]
-
+let question1Pick = questions[Math.floor(Math.random() * 11)];
 
 
 $(document).ready(function () {
+ //Shuffle Sort for Multiple Choice Questions
+ function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+  //multiple choice questions
+  document.getElementById("question1").innerHTML = question1Pick;
+  if(question1Pick === questions[0]){
+    shuffle(q1Choice);
+    document.getElementById("answer1").innerHTML = q1Choice[0];
+    document.getElementById("answer2").innerHTML = q1Choice[1];
+    document.getElementById("answer3").innerHTML = q1Choice[2];
+    document.getElementById("answer4").innerHTML = q1Choice[3];
+  } else if (question1Pick === questions[1]) {
+    shuffle(q2Choice);
+    document.getElementById("answer1").innerHTML = q2Choice[0];
+    document.getElementById("answer2").innerHTML = q2Choice[1];
+    document.getElementById("answer3").innerHTML = q2Choice[2];
+    document.getElementById("answer4").innerHTML = q2Choice[3];
+  }
+  else if (question1Pick === questions[2]) {
+    shuffle(q3Choice);
+    document.getElementById("answer1").innerHTML = q3Choice[0];
+    document.getElementById("answer2").innerHTML = q3Choice[1];
+    document.getElementById("answer3").innerHTML = q3Choice[2];
+    document.getElementById("answer4").innerHTML = q3Choice[3];
+  }
+  else if (question1Pick === questions[3]) {
+    shuffle(q4Choice);
+    document.getElementById("answer1").innerHTML = q4Choice[0];
+    document.getElementById("answer2").innerHTML = q4Choice[1];
+    document.getElementById("answer3").innerHTML = q4Choice[2];
+    document.getElementById("answer4").innerHTML = q4Choice[3];
+  }
+  else if (question1Pick === questions[4]) {
+    shuffle(q5Choice);
+    document.getElementById("answer1").innerHTML = q4Choice[0];
+    document.getElementById("answer2").innerHTML = q4Choice[1];
+    document.getElementById("answer3").innerHTML = q4Choice[2];
+    document.getElementById("answer4").innerHTML = q4Choice[3];
+  }
+  else if (question1Pick === questions[5]) {
+    shuffle(q6Choice);
+    document.getElementById("answer1").innerHTML = q5Choice[0];
+    document.getElementById("answer2").innerHTML = q5Choice[1];
+    document.getElementById("answer3").innerHTML = q5Choice[2];
+    document.getElementById("answer4").innerHTML = q5Choice[3];
+  }
+  else if (question1Pick === questions[6]) {
+    shuffle(q7Choice);
+    document.getElementById("answer1").innerHTML = q6Choice[0];
+    document.getElementById("answer2").innerHTML = q6Choice[1];
+    document.getElementById("answer3").innerHTML = q6Choice[2];
+    document.getElementById("answer4").innerHTML = q6Choice[3];
+  }
+  else if (question1Pick === questions[7]) {
+    shuffle(q8Choice);
+    document.getElementById("answer1").innerHTML = q8Choice[0];
+    document.getElementById("answer2").innerHTML = q8Choice[1];
+    document.getElementById("answer3").innerHTML = q8Choice[2];
+    document.getElementById("answer4").innerHTML = q8Choice[3];
+  }
+  else if (question1Pick === questions[8]) {
+    shuffle(q9Choice);
+    document.getElementById("answer1").innerHTML = q9Choice[0];
+    document.getElementById("answer2").innerHTML = q9Choice[1];
+    document.getElementById("answer3").innerHTML = q9Choice[2];
+    document.getElementById("answer4").innerHTML = q9Choice[3];
+  }
+  else if (question1Pick === questions[9]) {
+    shuffle(q10Choice);
+    document.getElementById("answer1").innerHTML = q10Choice[0];
+    document.getElementById("answer2").innerHTML = q10hoice[1];
+    document.getElementById("answer3").innerHTML = q10Choice[2];
+    document.getElementById("answer4").innerHTML = q10Choice[3];
+  }
+  else if (question1Pick === questions[10]) {
+    shuffle(q11Choice);
+    document.getElementById("answer1").innerHTML = q11Choice[0];
+    document.getElementById("answer2").innerHTML = q11Choice[1];
+    document.getElementById("answer3").innerHTML = q11Choice[2];
+    document.getElementById("answer4").innerHTML = q11Choice[3];
+  }
+
+  timer();
+    
+});
+
+//timer function
+function timer()
+{
+    count = count-1;
+    
+    if (count <= 0){
+        clearInterval(counter);
 
 
 
-
-
-
-
-
-
-
-};
-
-
-
-// let guessValue = Math.floor(Math.random() * 120) + 19;
-
-// let currentValue = 0;
-
-// let win = 0;
-// let loss = 0; 
-
-// let gem1 = Math.floor(Math.random() * 12) + 1;
-// console.log(gem1);
-
-// let gem2 = Math.floor(Math.random() * 12) + 1;
-// console.log(gem2);
-
-// let gem3 = Math.floor(Math.random() * 12) + 1;
-// console.log(gem3);
-
-// let gem4 = Math.floor(Math.random() * 12) + 1;
-// console.log(gem4);
-
-// $(document).ready(function(){
-//     document.getElementById("valueToGuess").innerHTML = guessValue;
-// });    
-
-// const gem1Add = () => {
-//     document.getElementById("currentValue").innerHTML = currentValue += gem1;
-// };
-// const gem2Add = () => {
-//     document.getElementById("currentValue").innerHTML = currentValue += gem2;
-// };
-// const gem3Add = () => {
-//     document.getElementById("currentValue").innerHTML = currentValue += gem3;
-// };
-// const gem4Add = () => {
-//     document.getElementById("currentValue").innerHTML = currentValue += gem4;
-// };
-
-
-// window.setInterval(function(){
-//   if(currentValue === guessValue){
-//       win += 1;
-//       $('#wins').text(win);
-//       console.log(win);
-//       guessValue = Math.floor(Math.random() * 120) + 19;
-//       currentValue = 0;
-//       document.getElementById("valueToGuess").innerHTML = guessValue;
-//       document.getElementById("currentValue").innerHTML = currentValue;
-//   } else if(currentValue > guessValue){
-//       loss += 1;
-//       $('#losses').text(loss);
-//       console.log(loss);
-//       guessValue = Math.floor(Math.random() * 120) + 19;
-//       currentValue = 0;
-//       document.getElementById("valueToGuess").innerHTML = guessValue;
-//       document.getElementById("currentValue").innerHTML = currentValue;
-//   }
-// }, 10);
+console.log(questions[0]);
 
 
 
